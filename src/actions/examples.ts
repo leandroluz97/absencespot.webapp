8f39cf98f6e7574484f0a9497f648850162c9871
leandroluz97 <leandrodaluz97@gmail.com>
2023-08-27 01:55:55 +0100

    added actions types

--- src/actions/examples.ts
+++ src/actions/examples.ts
@@ -1,16 +1,23 @@
+import { AxiosResponse, AxiosError } from 'axios';
+import { axios } from '@/lib/axios';
 import { AppDispatch } from '@/reducers';
 import { exampleActions } from '@/reducers/example';
+import * as urls from '@/constants/webapi/example';
+
+interface Example {}
 
 export const GetExamples = () => {
     return async (dispatch: AppDispatch) => {
         dispatch(exampleActions.decrement());
-        // return axios
-        //     .get('url')
-        //     .then((response) => {
-        //         dispatch(exampleActions.decrement());
-        //     })
-        //     .catch((error) => {
-        //         dispatch(exampleActions.decrement());
-        //     });
+        return axios
+            .get(urls.getExamplesUrl())
+            .then((response: AxiosResponse<Example>) => {
+                dispatch(exampleActions.decrement());
+                console.log(response);
+            })
+            .catch((error: AxiosError<Example>) => {
+                dispatch(exampleActions.decrement());
+                console.log(error);
+            });
     };
 };
