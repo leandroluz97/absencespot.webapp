f1b004ee6f3732bcb91ed58142d727e5c0bec1ea
leandroluz97 <leandrodaluz97@gmail.com>
2023-08-27 01:55:11 +0100

    added axios config

---
+++ src/lib/axios.ts
@@ -0,0 +1,32 @@
+import Axios, { AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios';
+import { storageService } from '@/constants/utils/storage';
+
+const API_URL = 'https://localhost:7100/api/v1';
+
+const authRequestInterceptor = (
+    config: InternalAxiosRequestConfig<AxiosRequestConfig>
+): InternalAxiosRequestConfig => {
+    const token = storageService.getToken();
+    if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+    }
+    config.headers.Accept = 'application/json';
+    return config;
+};
+
+export const axios = Axios.create({
+    baseURL: API_URL,
+});
+
+axios.interceptors.request.use(authRequestInterceptor, (error) => Promise.reject(error));
+axios.interceptors.response.use(
+    (response) => response.data,
+    (error) => {
+        if (error.response.status === 401) {
+            storageService.clearToken();
+            window.location.href = '/auth/login';
+        }
+
+        throw error.response;
+    }
+);
