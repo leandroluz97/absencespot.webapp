d3bcea20deb7800be31568ce8da648b1af0c4e3c
leandroluz97 <leandrodaluz97@gmail.com>
2023-08-31 23:11:34 +0100

    added radix accordion

--- src/_common/layout/MainLayout.tsx
+++ src/_common/layout/MainLayout.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import {
     Bell,
     Building2,
     Calendar,
+    ChevronDownIcon,
     Clock3,
     Crown,
     FilePieChart,
@@ -13,16 +14,43 @@ import {
     Users2,
 } from 'lucide-react';
 import absenceSpotLogo from '@/assets/absence-spot-logo.svg';
+import * as Accordion from '@radix-ui/react-accordion';
 import { Link, NavLink, Outlet, useLocation, useParams } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
+
+interface Props {
+    children: ReactNode;
+}
+
+const AccordionTrigger = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+    <Accordion.Header className="AccordionHeader">
+        <Accordion.Trigger
+            className={twMerge(
+                'w-full flex justify-between items-center text-primary-900 bg-slate-100 pr-2 rounded',
+                className
+            )}
+            {...props}
+            ref={forwardedRef}
+        >
+            {children}
+            <ChevronDownIcon size={26} className="" aria-hidden />
+        </Accordion.Trigger>
+    </Accordion.Header>
+));
+
+const AccordionContent = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+    <Accordion.Content className={twMerge('ml-6', className)} {...props} ref={forwardedRef}>
+        <div className="ml-6">{children}</div>
+    </Accordion.Content>
+));
 
 export const MainLayout = () => {
     const { id } = useParams();
     const { pathname } = useLocation();
     return (
         <React.Fragment>
-            <section className="flex bg-slate-100">
-                <article className="h-screen">
+            <section className=" h-screen flex bg-slate-100">
+                <article className="h-screen ">
                     <nav className="h-full w-64 p-4 space-y-6 flex flex-col bg-white">
                         <Link to={`/c/${id}/dashboard`} className="">
                             <img src={absenceSpotLogo} alt="absence spot" className="w-40" />
@@ -94,36 +122,158 @@ export const MainLayout = () => {
                                     </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink
-                                        to={`/c/${id}/request`}
-                                        className={({ isActive }) =>
-                                            `group text-sm flex gap-2 font-medium p-3 rounded hover:font-medium hover:bg-slate-100 hover:text-primary-900 ${
-                                                isActive
-                                                    ? ' text-primary-900 bg-slate-100'
-                                                    : 'text-red-300'
-                                            }`
-                                        }
+                                    <Accordion.Root
+                                        className="AccordionRoot"
+                                        type="single"
+                                        defaultValue="item-1"
+                                        collapsible
                                     >
-                                        <FilePieChart
-                                            size={20}
-                                            className={twMerge(
-                                                'text-slate-400 group-hover:text-primary-900 ',
-                                                pathname.includes('request')
-                                                    ? 'text-primary-900'
-                                                    : ''
-                                            )}
-                                        />
-                                        <span
-                                            className={twMerge(
-                                                'text-slate-500 group-hover:text-primary-900 ',
-                                                pathname.includes('request')
-                                                    ? 'text-primary-900'
-                                                    : ''
-                                            )}
+                                        <Accordion.Item className="AccordionItem" value="item-1">
+                                            <AccordionTrigger>
+                                                <NavLink
+                                                    to={`/c/${id}/request`}
+                                                    className={({ isActive }) =>
+                                                        `group text-sm flex gap-2 font-medium p-3 rounded hover:font-medium hover:bg-slate-100 hover:text-primary-900 ${
+                                                            isActive
+                                                                ? ' text-primary-900 bg-slate-100'
+                                                                : 'text-red-300'
+                                                        }`
+                                                    }
+                                                >
+                                                    <FilePieChart
+                                                        size={20}
+                                                        className={twMerge(
+                                                            'text-slate-400 group-hover:text-primary-900 ',
+                                                            pathname.includes('request')
+                                                                ? 'text-primary-900'
+                                                                : ''
+                                                        )}
+                                                    />
+                                                    <span
+                                                        className={twMerge(
+                                                            'text-slate-500 group-hover:text-primary-900 ',
+                                                            pathname.includes('request')
+                                                                ? 'text-primary-900'
+                                                                : ''
+                                                        )}
+                                                    >
+                                                        Request
+                                                    </span>
+                                                </NavLink>
+                                            </AccordionTrigger>
+                                            <div style={{ marginLeft: '26px' }}>
+                                                <Accordion.Content className="w-36">
+                                                    <NavLink
+                                                        to={`/c/${id}/request?filter=approved`}
+                                                        className={({ isActive }) =>
+                                                            `group text-sm flex gap-2 font-medium p-3 rounded hover:font-medium hover:bg-slate-100 hover:text-primary-900 ${
+                                                                isActive
+                                                                    ? ' text-primary-900'
+                                                                    : 'text-red-300'
+                                                            }`
+                                                        }
+                                                    >
+                                                        <span
+                                                            className={twMerge(
+                                                                'text-slate-500 group-hover:text-primary-900 ',
+                                                                pathname.includes('request')
+                                                                    ? 'text-primary-900'
+                                                                    : ''
+                                                            )}
+                                                        >
+                                                            Approved
+                                                        </span>
+                                                    </NavLink>
+                                                    <NavLink
+                                                        to={`/c/${id}/request?filter=approved`}
+                                                        className={({ isActive }) =>
+                                                            `group text-sm flex gap-2 font-medium p-3 rounded hover:font-medium hover:bg-slate-100 hover:text-primary-900 ${
+                                                                isActive
+                                                                    ? ' text-primary-900'
+                                                                    : 'text-red-300'
+                                                            }`
+                                                        }
+                                                    >
+                                                        <span
+                                                            className={twMerge(
+                                                                'text-slate-500 group-hover:text-primary-900 ',
+                                                                pathname.includes('pending')
+                                                                    ? 'text-primary-900'
+                                                                    : ''
+                                                            )}
+                                                        >
+                                                            Pending
+                                                        </span>
+                                                    </NavLink>
+                                                    <NavLink
+                                                        to={`/c/${id}/request?filter=approved`}
+                                                        className={({ isActive }) =>
+                                                            `group text-sm flex gap-2 font-medium p-3 rounded hover:font-medium hover:bg-slate-100 hover:text-primary-900 ${
+                                                                isActive
+                                                                    ? ' text-primary-900 '
+                                                                    : 'text-red-300'
+                                                            }`
+                                                        }
+                                                    >
+                                                        <span
+                                                            className={twMerge(
+                                                                'text-slate-500 group-hover:text-primary-900 ',
+                                                                pathname.includes('approved')
+                                                                    ? 'text-primary-900'
+                                                                    : ''
+                                                            )}
+                                                        >
+                                                            Reject
+                                                        </span>
+                                                    </NavLink>
+                                                </Accordion.Content>
+                                            </div>
+                                        </Accordion.Item>
+                                    </Accordion.Root>
+                                    {/* <Accordion.Root
+                                            className="AccordionRoot"
+                                            type="single"
+                                            defaultValue="item-1"
+                                            collapsible
                                         >
-                                            Request
-                                        </span>
-                                    </NavLink>
+                                            <Accordion.Item
+                                                className="AccordionItem"
+                                                value="item-1"
+                                            >
+                                                <AccordionTrigger>
+                                                    Is it accessible?
+                                                </AccordionTrigger>
+                                                <AccordionContent>
+                                                    Yes. It adheres to the WAI-ARIA design pattern.
+                                                </AccordionContent>
+                                            </Accordion.Item>
+
+                                            <Accordion.Item
+                                                className="AccordionItem"
+                                                value="item-2"
+                                            >
+                                                <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+                                                <AccordionContent>
+                                                    Yes. It's unstyled by default, giving you
+                                                    freedom over the look and feel.
+                                                </AccordionContent>
+                                            </Accordion.Item>
+
+                                            <Accordion.Item
+                                                className="AccordionItem"
+                                                value="item-3"
+                                            >
+                                                <AccordionTrigger>
+                                                    Can it be animated?
+                                                </AccordionTrigger>
+                                                <Accordion.Content className="AccordionContent">
+                                                    <div className="AccordionContentText">
+                                                        Yes! You can animate the Accordion with CSS
+                                                        or JavaScript.
+                                                    </div>
+                                                </Accordion.Content>
+                                            </Accordion.Item>
+                                        </Accordion.Root> */}
                                 </li>
                                 <li>
                                     <NavLink
