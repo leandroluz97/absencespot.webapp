3c915b4b0812ec11f98f39bc4316d696b8db43d8
leandroluz97 <leandrodaluz97@gmail.com>
2023-09-10 03:17:17 +0100

    added URLSearch class

---
+++ src/constants/utils/URLSearch.ts
@@ -0,0 +1,93 @@
+interface IQuery {
+    searchText: string;
+    pageSize: string;
+    pageNumber: string;
+    sortBy: string;
+    sortDesc: string;
+    date: string;
+    type: string;
+}
+
+type IEntries = 'pageSize' | 'searchText' | 'pageNumber' | 'sortBy' | 'sortDesc' | 'date' | 'type';
+
+export const urlQueries: IEntries[] = [
+    'searchText',
+    'pageSize',
+    'pageNumber',
+    'sortBy',
+    'sortDesc',
+    'date',
+    'type',
+];
+
+export class URLSearch {
+    static #entries: IEntries[];
+
+    private static searchParams: URLSearchParams;
+
+    static register(entries: IEntries[]) {
+        if (!Array.isArray(entries)) {
+            throw new Error(
+                `Registered 'queries ${entries}' is not an array of strings. Eg.: ["sortBy", "sortDesc", "status" ]`
+            );
+        }
+        this.#entries = entries;
+    }
+
+    static querySearchParams(): string {
+        return window.location.search;
+    }
+
+    static queries(): IQuery {
+        this.#getQueries();
+        const queries: IQuery = {} as IQuery;
+        this.#entries.forEach((entry: IEntries) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            queries[entry] = this[entry] as unknown as IEntries;
+        });
+        return queries;
+    }
+
+    static #getQueries() {
+        this.searchParams = new URLSearchParams(window.location.search);
+        this.#entries.forEach((entry: IEntries) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            this[entry] = this.searchParams.get(entry);
+        });
+    }
+
+    static set(entries: Record<string, string>): string {
+        const url = new URL(window.location.href);
+        Object.entries(entries).forEach(([key, value]) => url.searchParams.set(key, value));
+        return url.toString();
+    }
+
+    static delete(entries: string[]): string {
+        const url = new URL(window.location.href);
+        entries.forEach((entry) => url.searchParams.delete(entry));
+        return url.toString();
+    }
+
+    static reset({
+        remove,
+        initial,
+    }: {
+        remove: string[];
+        initial: Record<string, string>;
+    }): string {
+        const url = new URL(window.location.href);
+        if (remove) this.#exclude(url, remove);
+        if (initial) this.#setInitial(url, initial);
+        return url.toString();
+    }
+
+    static #exclude(url: URL, entriesToDelete: string[]) {
+        entriesToDelete.forEach((entry) => url.searchParams.delete(entry));
+    }
+
+    static #setInitial(url: URL, entries: Record<string, string>) {
+        Object.entries(entries).forEach(([key, value]) => url.searchParams.set(key, value));
+    }
+}
