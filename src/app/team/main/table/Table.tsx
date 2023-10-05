d331dcd76bbf98938a7f14058d8b6c04b8eb0b38
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-05 17:36:11 +0100

    added responsife

--- src/app/team/main/table/Table.tsx
+++ src/app/team/main/table/Table.tsx
@@ -8,7 +8,7 @@ export const TeamTable = () => {
             <div className="h-full overflow-auto">
                 {/* <TableHeader /> */}
                 {/* <div className="h-full overflow-y-auto grid grid-cols-5 w-100 space-y-[3px]"> */}
-                <div className="grid grid-cols-4 gap-[3px]">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[3px]">
                     {Array(40)
                         .fill(2)
                         .map((_, index) => (
