edbe128a03802f299ad8e2d56ec14586444bcdbd
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-06 15:39:38 +0100

    added LeaveTypeMain index file

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -2,6 +2,10 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from './_common/layout';
 import { General } from './general';
 import { User } from './user';
+import { Integration } from './integration';
+import { LeaveTypes } from './leave';
+import { WorkSchedule } from './schedule';
+import { Plans } from './plans';
 
 export const Settings = () => {
     return (
@@ -9,9 +13,10 @@ export const Settings = () => {
             <Route element={<Layout />}>
                 <Route index element={<General />} />
                 <Route path="user/*" element={<User />} />
-                <Route path="general" element={<p>test</p>} />
-                <Route path="general" element={<p>test</p>} />
-                <Route path="/:id/edit" element={<p>Edit</p>} />
+                <Route path="leave-types/*" element={<LeaveTypes />} />
+                <Route path="integration" element={<Integration />} />
+                <Route path="work-schedule/*" element={<WorkSchedule />} />
+                <Route path="plans" element={<Plans />} />
                 <Route path="" element={<p>404</p>} />
                 <Route path="*" element={<Navigate to="/c/5454-4353-534f-3ef6/dashboard" />} />
             </Route>
---
+++ src/app/settings/leave/main/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const LeaveTypeMain = () => {
+    return <div>LeaveTypeMain</div>;
+};
