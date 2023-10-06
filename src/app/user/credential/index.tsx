7871a910926d1d2665acddce64a57e67c2316936
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-06 20:26:19 +0100

    added user credentials entry routes

---
+++ src/app/user/credential/index.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { CredentialMain } from './main';
+import { CredentialNew } from './new';
+
+export const Credential = () => {
+    return (
+        <Routes>
+            <Route index element={<CredentialNew />} />
+            <Route path="new" element={<CredentialNew />} />
+            <Route path="/:id/edit" element={<p>Edit</p>} />
+            <Route path="" element={<p>404</p>} />
+            <Route path="*" element={<Navigate to="/c/5454-4353-534f-3ef6/dashboard" />} />
+        </Routes>
+    );
+};
