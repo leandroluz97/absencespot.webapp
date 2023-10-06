d34351168c5be78bcad10eabdad7b987bbc8d739
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-06 15:36:34 +0100

    update layout padding

--- src/app/settings/_common/layout.tsx
+++ src/app/settings/_common/layout.tsx
@@ -1,24 +1,40 @@
 import { SimpleLayout } from '@/_common/layout/SimpleLayout';
 import { Submenu } from '@/app/_common/submenu';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export const Layout = () => {
+    const { pathname } = useLocation();
     return (
         <SimpleLayout>
             <Submenu.Root>
                 <Submenu.Content>
                     <Submenu.List>
-                        <Submenu.Item to="">General</Submenu.Item>
-                        <Submenu.Item to="user">Users</Submenu.Item>
-                        <Submenu.Item to="leave-types">Leave Types</Submenu.Item>
-                        <Submenu.Item to="integration">Integration</Submenu.Item>
-                        <Submenu.Item to="work-schedule">Work Schedule</Submenu.Item>
-                        <Submenu.Item to="plans">Plans</Submenu.Item>
+                        <Submenu.Item to="" isActive={pathname.endsWith('settings')}>
+                            General
+                        </Submenu.Item>
+                        <Submenu.Item to="user" isActive={pathname.includes('user')}>
+                            Users
+                        </Submenu.Item>
+                        <Submenu.Item to="leave-types" isActive={pathname.includes('leave-types')}>
+                            Leave Types
+                        </Submenu.Item>
+                        <Submenu.Item to="integration" isActive={pathname.includes('integration')}>
+                            Integration
+                        </Submenu.Item>
+                        <Submenu.Item
+                            to="work-schedule"
+                            isActive={pathname.includes('work-schedule')}
+                        >
+                            Work Schedule
+                        </Submenu.Item>
+                        <Submenu.Item to="plans" isActive={pathname.includes('plans')}>
+                            Plans
+                        </Submenu.Item>
                     </Submenu.List>
                 </Submenu.Content>
             </Submenu.Root>
             <div className="flex-1 bg-white max-w-6xl rounded-lg h-auto md:h-full overflow-hidden p-2">
-                <div className="p-10 h-full overflow-y-auto text-sm">
+                <div className="p-2 md:p-10 h-full overflow-y-auto text-sm">
                     <Outlet />
                 </div>
             </div>
