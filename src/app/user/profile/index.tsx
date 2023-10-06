64f6143984539a877e4a44e6c9984409d29b55c3
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-06 22:45:36 +0100

    added profile entry route

---
+++ src/app/user/profile/index.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { ProfileMain } from './main';
+import { ProfileNew } from './new';
+
+export const Profile = () => {
+    return (
+        <Routes>
+            <Route index element={<ProfileMain />} />
+            <Route path="new" element={<ProfileNew />} />
+            <Route path="/:id/edit" element={<p>Edit</p>} />
+            <Route path="" element={<p>404</p>} />
+            <Route path="*" element={<Navigate to="/c/5454-4353-534f-3ef6/dashboard" />} />
+        </Routes>
+    );
+};
