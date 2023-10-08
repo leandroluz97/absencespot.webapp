e85edb834dcac050f04e7e3d2a449747cb4a49ff
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-08 04:25:18 +0100

    updated menu list padding

--- src/_common/Menu/MenuRoot.tsx
+++ src/_common/Menu/MenuRoot.tsx
@@ -12,7 +12,7 @@ export const MenuRoot = ({ children, ...props }: Props) => {
             {...props}
             className={({ isActive }) =>
                 twMerge(
-                    `group text-sm flex gap-2 font-medium p-2 rounded hover:font-medium hover:bg-slate-100 hover:text-primary-900 transition-all ease-in duration-300  ${
+                    `group text-sm flex gap-2 font-semibold p-3 rounded  hover:bg-slate-100 hover:text-primary-900 transition-all ease-in duration-300  ${
                         isActive ? ' text-primary-900 bg-slate-100' : ''
                     }`,
                     props.className
